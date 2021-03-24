eab800f24a9afaa5bb665b24d9ce5672c0ff616a
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-24 13:46:06 -0400

    Basic Functionality Updates
    - Changed var to const
    - table is now sorted
    - manifest looks for specific page
    - need to fire after page is loaded (need to sniff this out)
    - Still need to optimize element grabbing
    - Still need to add error catching

--- inject-content/content.js
+++ inject-content/content.js
@@ -1,17 +1,46 @@
-var strongs = document.querySelectorAll(`Strong`);
-
-var actionItemTitle = [...strongs].find(
+console.log(`I'm gonna sort your table!`);
+////////////////////////////////////////////
+// Use the strong elemnts to find the table...
+const strongs = document.querySelectorAll(`Strong`);
+const actionItemTitle = [...strongs].find(
   (item) => item.innerHTML === `Action Items`
 );
-// actionItemTitle.innerHTML = `The Test Worked!`;
+console.log(actionItemTitle);
+actionItemTitle.innerHTML = `The Test Worked!`;
 
-var tableBody = actionItemTitle.closest(`tbody`);
-var table = actionItemTitle.closest(`table`);
-var actionItemRows = tableBody.querySelectorAll(`tr`);
+// Grab the table, tablebody, and table rows
+const table = actionItemTitle.closest(`table`);
+const tableBody = actionItemTitle.closest(`tbody`);
+const actionItemRows = tableBody.querySelectorAll(`tr`);
 
-// Create new tablebody
-var newTableBody = document.createElement(`tbody`);
+////////////////////////////////////////////
+// Sorting the table contents
+console.log(actionItemRows);
+// Make the existing table rows into an array
+const newTableRows = [...actionItemRows];
+// Remove the header elements
+newTableRows.splice(0, 2);
+console.log(`newTableRows child says:`, newTableRows[0].children[0].innerHTML);
+// Sort the rows by the content of the first column (the sequence ID)
+newTableRows.sort((a, b) => a.children[0].innerHTML - b.children[0].innerHTML);
+
+console.log(
+  `newTableRows sorted child says:`,
+  newTableRows[0].children[0].innerHTML
+);
+
+////////////////////////////////////////////
+// Create the new table
+// Create new tablebody element.
+const newTableBody = document.createElement(`tbody`);
+// Insert the header elements
 newTableBody.insertAdjacentElement(`beforeend`, actionItemRows[0]);
 newTableBody.insertAdjacentElement(`beforeend`, actionItemRows[1]);
+// Insert the sorted rows
+newTableRows.forEach((el) =>
+  newTableBody.insertAdjacentElement(`beforeend`, el)
+);
 
-// Sort rows
+// Insert newly-built tablebody
+tableBody.remove();
+table.insertAdjacentElement(`afterbegin`, newTableBody);
